989adf749e2783d12dd9dd969923a55032b37f50
Maximiliano Stein <maxi_stein23@hotmail.com>
2022-12-03 11:49:09 -0300

    Translated main to enlgish

--- main.c
+++ main.c
@@ -6,49 +6,48 @@
 
 typedef struct{
     int id;
-    char marca[DIM];
-    char modelo[DIM];
-    float precio;
-    float descuento;
-}registro;
+    char brand[DIM];
+    char model[DIM];
+    float price;
+    float discount;
+}registry;
 
 
-void getData(char* nomArchivo,registro* data){
-    FILE* file=fopen(nomArchivo,"rt");
+void getData(char* nameArchive,registry* data){
+    FILE* file=fopen(nameArchive,"rt");
     int i=0;
-    char titulos[150];
-    char stringTokenizar[250];
+    char title[150];
+    char stringTokenize[250];
     char* token;
 
     if(file==NULL){
         perror("Error");
     }
     else{
-        fgets(titulos,150,file); //lo utilizo unicamente para poder empezar a leer los datos correctamente
+        fgets(title,150,file); //used only to dump the first line and start reading the actual data
 
-        fgets(stringTokenizar,250,file);
-        token=strtok(stringTokenizar,",");
+        fgets(stringTokenize,250,file);
+        token=strtok(stringTokenize,",");
         data[i].id=atoi(token);
 
         while(!feof(file)){
 
             while(token!=NULL){
                 token=strtok(NULL,",");
-                strcpy(data[i].marca,token);
+                strcpy(data[i].brand,token);
                 token=strtok(NULL,",");
-                strcpy(data[i].modelo,token);
+                strcpy(data[i].model,token);
                 token=strtok(NULL,",");
-                data[i].precio=atof(token);
-                //printf("Precio al cargarlo: %f \n",data[i].precio);
+                data[i].price=atof(token);
                 token=strtok(NULL,",");
-                data[i].descuento=atof(token);
+                data[i].discount=atof(token);
                 i++;
                 token=strtok(NULL,",");
             }
 
             if(!feof(file)){
-                fgets(stringTokenizar,250,file);
-                token=strtok(stringTokenizar,",");
+                fgets(stringTokenize,250,file);
+                token=strtok(stringTokenize,",");
                 data[i].id=atoi(token);
             }
         }
@@ -56,39 +55,38 @@ void getData(char* nomArchivo,registro* data){
     fclose(file);
 }
 
-int getMejorModelo(registro* data){
+int getBestModel(registry* data){
     int j;
-    float mejorPrecio;
-    int indiceMejor;
+    float bestPrice;
+    int bestIndex;
     for(j=0;j<BUFFER;j++){
         if(j==0){
-            mejorPrecio=data[j].precio*(1-data[j].descuento);
-            indiceMejor=j;
+            bestPrice=data[j].price*(1-data[j].discount);
+            bestIndex=j;
         }
         else{
-            if(data[j].precio*(1-data[j].descuento)<mejorPrecio){
-                mejorPrecio=data[j].precio*(1-data[j].descuento);
-                indiceMejor=j;
+            if(data[j].price*(1-data[j].discount)<bestPrice){
+                bestPrice=data[j].price*(1-data[j].discount);
+                bestIndex=j;
             }
         }
-        printf("ID:%d - MARCA:%s - MODELO:%s \nPRECIO:%.2f \nDESCUENTO:%%%.2f \nPrecio al Contado:$%.2f\n\n",data[j].id,data[j].marca,data[j].modelo,data[j].precio,data[j].descuento,data[j].precio*(1-data[j].descuento));
+        printf("ID:%d - BRAND:%s - MODEL:%s \nPRICE:$%.2f \nDISCOUNT:%%%.2f \nPRICE CASH-IN-HAND:$%.2f\n\n",data[j].id,data[j].brand,data[j].model,data[j].price,data[j].discount,data[j].price*(1-data[j].discount));
     }
-    return data[indiceMejor].id;
+    return data[bestIndex].id;
 }
 
-void mostrarOrdenado(registro* data){
+void showOrdered(registry* data){
     int k,h,it1,it2;
 
-    registro aux;
-    float precios[BUFFER];
+    registry aux;
+    float prices[BUFFER];
 
     for(k=0;k<BUFFER;k++){
-        precios[k]=data[k].precio;
+        prices[k]=data[k].price;
     }
     for(it1=0;it1<BUFFER;it1++){
         for(it2=0;it2<BUFFER-1;it2++){
-            if(data[it2].precio>data[it2+1].precio){
-                //printf("Cambio %.2f por %.2f \n",data[it2].precio,data[it2+1].precio);
+            if(data[it2].price>data[it2+1].price){
                 aux=data[it2+1];
                 data[it2+1]=data[it2];
                 data[it2]=aux;
@@ -96,18 +94,18 @@ void mostrarOrdenado(registro* data){
         }
     }
 
-    printf("Precios ordenados de Menor a Mayor: \n");
+    printf("Prices ordered from lowest to highest: \n");
     for(h=0;h<BUFFER;h++){
-        printf("%.2f - %s - %s \n",data[h].precio,data[h].marca,data[h].modelo);
+        printf("%.2f - %s - %s \n",data[h].price,data[h].brand,data[h].model);
     }
 }
 
 int main(){
     int id;
-    registro item[BUFFER];
+    registry item[BUFFER];
     getData("productos.txt",item);
-    id=getMejorModelo(item);
-    printf("El mejor precio al contado es: %d \n\n",id);
-    mostrarOrdenado(item);
+    id=getBestModel(item);
+    printf("The best price for cash-in-hand is: %d \n\n",id);
+    showOrdered(item);
     return 0;
 }
